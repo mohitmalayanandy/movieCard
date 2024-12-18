dfe69e6f7875d6c3e9fc8be2281dec0cef086641
Mohit Malaya Nandy <mohit78nandy@gmail.com>
2024-12-19 00:41:13 +0530

    NetflixSeries.jsx file is created.
    NetflixSeries.jsx file is created and moved the details from App.jsx to Netflix Series.jsx

---
+++ src/components/NetflixSeries.jsx
@@ -0,0 +1,19 @@
+const NetflixSeries = () => {
+    return(
+      <div>
+          <div>
+            <img src="https://upload.wikimedia.org/wikipedia/en/7/75/Pushpa_-_The_Rise_%282021_film%29.jpg"
+              alt="Pushpa 2 banner"
+              height="250px"
+              weight="250px" />
+          </div>
+          <div>
+            <h1><b>Name:</b> Pushpa 2</h1>
+            <h2><b>Ratting:</b> 8.9</h2>
+            <p><b>Summary:</b> Pushpa 2 has unleashed madness at the box office, with the film standing at a massive 605.25 crore in 6 days. It would not be an exaggeration to say the film might enter the 1000 crore club by the beginning of the second weekend. The film is all set to bring magical numbers on weekdays as well.Pushpa 2 Box Office Day 6On the sixth day</p>
+            <button>Not Available</button>
+          </div>
+        </div>
+    )
+  }
+  export default NetflixSeries
