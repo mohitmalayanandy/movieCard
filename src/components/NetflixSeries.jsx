5e350c73f35068b25b6f08b87ddf570b176e5b8a
Mohit Malaya Nandy <mohit78nandy@gmail.com>
2024-12-19 10:00:46 +0530

    Normal If Case
    The Code is repeated multiple times so it is a drawback of applying condition in this way.

--- src/components/NetflixSeries.jsx
+++ src/components/NetflixSeries.jsx
@@ -6,21 +6,46 @@ const NetflixSeries = () => {
     const ratting = "8.9";
     const summary = "Pushpa 2 has unleashed madness at the box office, with the film standing at a massive 605.25 crore in 6 days. It would not be an exaggeration to say the film might enter the 1000 crore club by the beginning of the second weekend. The film is all set to bring magical numbers on weekdays as well.Pushpa 2 Box Office Day 6On the sixth day";
     const watch_url = "https://youtu.be/1kVK0MZlbI4?si=VMxcC1GikWAvW6Cb";
-    return(
-      <div>
-          <div>
-            <img src= {image}
-              alt= {id}
-              height="250px"
-              weight="250px" />
-          </div>
-          <div>
-            <h1><b>Name:</b> {name} </h1>
-            <h2><b>Ratting:</b> {ratting} </h2>
-            <p><b>Summary:</b> {summary} </p>
-            <a href= {watch_url} ><button>Watch Now</button></a>
-          </div>
-        </div>
+    let age = 19;
+    // Normal If Case
+    if (age >= 18) {
+        return (
+            <>
+                <div>
+                    <div>
+                        <img src={image}
+                            alt={id}
+                            height="250px"
+                            weight="250px" />
+                    </div>
+                    <div>
+                        <h1><b>Name:</b> {name} </h1>
+                        <h2><b>Ratting:</b> {ratting} </h2>
+                        <p><b>Summary:</b> {summary} </p>
+                        <a href={watch_url} target="_blank" ><button>Watch Now</button></a>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
+    return (
+        <>
+            <div>
+                <div>
+                    <img src={image}
+                        alt={id}
+                        height="250px"
+                        weight="250px" />
+                </div>
+                <div>
+                    <h1><b>Name:</b> {name} </h1>
+                    <h2><b>Ratting:</b> {ratting} </h2>
+                    <p><b>Summary:</b> {summary} </p>
+                    <button>Not Available</button>
+                </div>
+            </div>
+        </>
     )
-  }
-  export default NetflixSeries
+}
+export default NetflixSeries
