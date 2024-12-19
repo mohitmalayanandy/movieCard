a1759c6c9b987a7fb0274a568e380e63fc4a75b1
Mohit Malaya Nandy <mohit78nandy@gmail.com>
2024-12-19 09:38:59 +0530

    Used Dynamic Values / Variable.
    Added any variables of your component in jsx using { } .

--- src/components/NetflixSeries.jsx
+++ src/components/NetflixSeries.jsx
@@ -1,17 +1,24 @@
 const NetflixSeries = () => {
+
+    const image = "https://upload.wikimedia.org/wikipedia/en/7/75/Pushpa_-_The_Rise_%282021_film%29.jpg";
+    const id = "pushpa2";
+    const name = "Pushpa 2";
+    const ratting = "8.9";
+    const summary = "Pushpa 2 has unleashed madness at the box office, with the film standing at a massive 605.25 crore in 6 days. It would not be an exaggeration to say the film might enter the 1000 crore club by the beginning of the second weekend. The film is all set to bring magical numbers on weekdays as well.Pushpa 2 Box Office Day 6On the sixth day";
+    const watch_url = "https://youtu.be/1kVK0MZlbI4?si=VMxcC1GikWAvW6Cb";
     return(
       <div>
           <div>
-            <img src="https://upload.wikimedia.org/wikipedia/en/7/75/Pushpa_-_The_Rise_%282021_film%29.jpg"
-              alt="Pushpa 2 banner"
+            <img src= {image}
+              alt= {id}
               height="250px"
               weight="250px" />
           </div>
           <div>
-            <h1><b>Name:</b> Pushpa 2</h1>
-            <h2><b>Ratting:</b> 8.9</h2>
-            <p><b>Summary:</b> Pushpa 2 has unleashed madness at the box office, with the film standing at a massive 605.25 crore in 6 days. It would not be an exaggeration to say the film might enter the 1000 crore club by the beginning of the second weekend. The film is all set to bring magical numbers on weekdays as well.Pushpa 2 Box Office Day 6On the sixth day</p>
-            <button>Not Available</button>
+            <h1><b>Name:</b> {name} </h1>
+            <h2><b>Ratting:</b> {ratting} </h2>
+            <p><b>Summary:</b> {summary} </p>
+            <a href= {watch_url} ><button>Watch Now</button></a>
           </div>
         </div>
     )
