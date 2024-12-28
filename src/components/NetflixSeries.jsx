f0c6000a244480afb963722e997696716a60efa8
Mohit Malaya Nandy <mohitmalayanandy@gmail.com>
2024-12-28 17:27:13 +0530

    Looping In jsx
    Used map( ) to iterate each object from the json file to return the separate movies by looping.

--- src/components/NetflixSeries.jsx
+++ src/components/NetflixSeries.jsx
@@ -1,27 +1,32 @@
-// Default Export
-const NetflixSeries = () => {
+import seriesData from "../api/seriesData.json";
 
-    const image = "https://upload.wikimedia.org/wikipedia/en/7/75/Pushpa_-_The_Rise_%282021_film%29.jpg";
-    const id = "pushpa2";
-    const name = "Pushpa 2";
-    const ratting = "8.9";
-    const summary = "Pushpa 2 has unleashed madness at the box office, with the film standing at a massive 605.25 crore in 6 days. It would not be an exaggeration to say the film might enter the 1000 crore club by the beginning of the second weekend. The film is all set to bring magical numbers on weekdays as well.Pushpa 2 Box Office Day 6On the sixth day";
-    const watch_url = "https://youtu.be/1kVK0MZlbI4?si=VMxcC1GikWAvW6Cb";
-    return(
-      <div>
-          <div>
-            <img src= {image}
-              alt= {id}
-              height="250px"
-              weight="250px" />
-          </div>
-          <div>
-            <h1><b>Name:</b> {name} </h1>
-            <h2><b>Ratting:</b> {ratting} </h2>
-            <p><b>Summary:</b> {summary} </p>
-            <a href= {watch_url} target="_blank" ><button>Watch Now</button></a>
-          </div>
-        </div>
-    )
-  }
-  export default NetflixSeries
+const NetflixSeries = () => {
+  return (
+    <ul>
+      {seriesData.map((currElement) => {
+        return (
+          <li>
+            <div>
+              <div>
+                <img src={currElement.img_url}
+                  alt={currElement.id}
+                  height="250px"
+                  weight="250px" />
+              </div>
+              <div>
+                <h1><b>Name:</b> {currElement.name} </h1>
+                <h2><b>Ratting:</b> {currElement.rating} </h2>
+                <p><b>Summary:</b> {currElement.description} </p>
+                <p>Cast: {currElement.cast}</p>
+                <p>Genre: {currElement.genre} </p>
+                <a href={currElement.watch_url} target="_blank" ><button>Watch Now</button></a>
+              </div>
+            </div>
+          </li>
+        )
+      })}
+    </ul>
+
+  )
+}
+export default NetflixSeries
