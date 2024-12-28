57e1024ec81772898a1209851228d8fbe35dd16f
Mohit Malaya Nandy <mohitmalayanandy@gmail.com>
2024-12-28 18:18:09 +0530

    Destructuring of the props.
    destructured the props and fixed the duplication of the images on the server, which created confusion about the original file path by removing the /public.

--- src/components/SeriesCard.jsx
+++ src/components/SeriesCard.jsx
@@ -1,20 +1,21 @@
 const SeriesCard = ({props}) => {
+  const {img_url, id, name, rating, description, cast, genre, watch_url} = props
     return(
         <li>
             <div>
               <div>
-                <img src={props.img_url}
-                  alt={props.id}
+                <img src={img_url}
+                  alt={id}
                   height="250px"
                   weight="250px" />
               </div>
               <div>
-                <h1><b>Name:</b> {props.name} </h1>
-                <h2><b>Ratting:</b> {props.rating} </h2>
-                <p><b>Summary:</b> {props.description} </p>
-                <p>Cast: {props.cast.join(", ")}</p>
-                <p>Genre: {props.genre.join(", ")} </p>
-                <a href={props.watch_url} target="_blank" ><button>Watch Now</button></a>
+                <h1><b>Name:</b> {name} </h1>
+                <h2><b>Ratting:</b> {rating} </h2>
+                <p><b>Summary:</b> {description} </p>
+                <p>Cast: {cast.join(", ")}</p>
+                <p>Genre: {genre.join(", ")} </p>
+                <a href={watch_url} target="_blank" ><button>Watch Now</button></a>
               </div>
             </div>
           </li>
